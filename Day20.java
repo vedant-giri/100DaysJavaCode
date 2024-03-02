18cb437265c01b355ab54906db5db23791011540
Vedant Giri <113013212+vedant-giri@users.noreply.github.com>
2024-03-02 14:50:51 +0530

    Day 60 Code

---
+++ Day20.java
@@ -0,0 +1,34 @@
+// Juggler Sequence is a series of integers in which the first term starts with a positive integer number a and the remaining terms are generated from the immediate previous term using the below recurrence relation
+
+class Solution {
+    static List<Integer> jugglerSequence(int N) {
+        // code here
+        List<Integer> ans = new ArrayList<>();
+        ans.add(N);
+        ans.addAll(sequence(N));
+        ans.remove(ans.size() - 1);
+        return ans;
+
+    }
+
+    static List<Integer> sequence(int N) {
+        if (N == 1) {
+            List<Integer> ans = new ArrayList<>();
+            ans.add(1);
+            return ans;
+        }
+        List<Integer> ans = new ArrayList<>();
+
+        if (N % 2 == 0) {
+            N = (int) Math.sqrt(N);
+            ans.add(N);
+        } else {
+            N = (int) Math.pow(N, 1.5);
+            ans.add(N);
+        }
+
+        ans.addAll(sequence(N));
+
+        return ans;
+    }
+}
