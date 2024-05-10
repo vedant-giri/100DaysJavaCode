58595c43158bac0f5a7d7ca6f47162fb5893feac
Vedant Giri <113013212+vedant-giri@users.noreply.github.com>
2024-05-10 09:38:02 +0530

    Day 88 Code

---
+++ Day88.java
@@ -0,0 +1,32 @@
+// Given an array of integers arr, the length of the array n, and an integer k, find all the unique combinations in arr where the sum of the combination is equal to k. Each number can only be used once in a combination.
+
+class Solution {
+    public List<List<Integer>> CombinationSum2(int arr[], int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(arr);
+        subset(arr, n, k, new ArrayList<>(), result, 0, 0);
+        return result;
+    }
+
+    private void subset(int arr[], int n, int k, List<Integer> current, List<List<Integer>> result, int sum,
+            int index) {
+        if (sum > k) {
+            return;
+        }
+
+        if (sum == k) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = index; i < n; i++) {
+            if (i > index && arr[i] == arr[i - 1]) {
+                continue; // Skip duplicates
+            }
+
+            current.add(arr[i]);
+            subset(arr, n, k, current, result, sum + arr[i], i + 1);
+            current.remove(current.size() - 1);
+        }
+    }
+}
